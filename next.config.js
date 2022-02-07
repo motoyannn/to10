382d9017c22072376c4264e5a8c0d006c53ef225
motoyannn <motoya.kon@gmail.com>
2022-02-07 22:40:33 +0900

    feat: modify build settings

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: 'docs',
   webpack(config, options) {
     config.resolve.alias['@'] = path.join(__dirname, 'src')
     return config
